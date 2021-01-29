1ac9762b958e18ed55d2e7d119d626c4bf07e891
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2021-01-29 22:46:33 +0900

    正常にトークンを送信できるようになりました

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pyotp
 import json
 import discord
+import os
+import sys
+import pathlib
 
 available_services = {"Discord", "Google"}
 send_services = {"Discord"}
@@ -15,30 +18,42 @@ class TokenIsNotFound(Exception):
 
 
 class AuthData:
-    def __init__(self, service, file_address='AUTH_DATA'):
+    def __init__(self, service, file_address='AUTH_DATA.json'):
         if service not in available_services:
             raise ServiceIsNotFound(f"not found service name is {service}")
 
         self.file_address = file_address
         self.service = service
+        self.p_lib = pathlib.Path(file_address)
         self.auth_key = None
         self.read_key()
 
     def set_key(self):
         token = input(f"Please type {self.service} key >> ").replace(" ", "")
         self.auth_key = token
+        json_data = dict()
         with open(self.file_address, "w") as f:
-            json_data = json.load(f)
+            try:
+                json_data = json.load(f)
+            except:
+                pass
             json_data[self.service] = token
             json.dumps(json_data)
 
     def read_key(self):
+
+        if not os.path.exists(self.file_address):
+            self.p_lib.touch()
+
         with open(self.file_address, "r") as f:
-            json_data = json.load(f)
             try:
+                json_data = json.load(f)
                 self.auth_key = json_data[self.service]
             except KeyError:
                 self.auth_key = None
+            except json.JSONDecodeError:
+                self.auth_key = None
+
         if self.auth_key is None:
             self.set_key()
         return self.auth_key
@@ -51,7 +66,7 @@ class AuthData:
 
 
 class Totp(AuthData):
-    def __init__(self, service, file_address='AUTH_DATA'):
+    def __init__(self, service, file_address='AUTH_DATA.json'):
         super(Totp, self).__init__(service, file_address)
         if service not in available_services - send_services:
             raise ServiceIsNotFound
@@ -65,7 +80,7 @@ class Totp(AuthData):
     def get_token(self):
         key = self.read_key()
         totp = pyotp.TOTP(key)
-        return totp
+        return totp.now()
 
     def get_token_string(self):
         token = self.get_token()
@@ -95,7 +110,7 @@ if __name__ == '__main__':
 
     @client.event
     async def on_ready():
-        pass
+        print("login success")
 
 
     @client.event
@@ -103,7 +118,10 @@ if __name__ == '__main__':
         if message.author.bot:
             return
         if message.content == '/google':
-            await message.channel.send(google_instance.get_token_string())
+            send_text = google_instance.get_token_string()
+            await message.channel.send(send_text)
 
 
-    client.run(discord_instance.get_key)
+    _ = discord_instance.get_key
+
+    client.run(discord_instance.get_key())
